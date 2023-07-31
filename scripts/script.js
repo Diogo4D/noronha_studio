f0af02d04629229776a85ad63ed762c0673e5960
Diogo Noronha da Costa Pinto <noronhadiogo319@gmail.com>
2023-07-30 21:34:29 -0300

    .
    principal,
    scrips

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,19 @@ const myObserver = new IntersectionObserver( (event) => {
 elements.forEach( (element) => {
     myObserver.observe(element);
 } )
+
+// menu responsivo
+
+const menu_responsivo = document.querySelector(".menu_respo");
+const close_menu = document.querySelector("#close_menu");
+const div_menu_responsivo = document.querySelector(".menu_responsivo")
+
+menu_responsivo.addEventListener("click", () => {
+    // div_menu_responsivo.classList.toggle( "menu_responsivo" );
+    div_menu_responsivo.classList.toggle( "menu_ativo" );
+})
+
+close_menu.addEventListener("click", () => {
+    // div_menu_responsivo.classList.toggle( "menu_responsivo" );
+    div_menu_responsivo.classList.toggle( "menu_ativo" );
+})
